1a2064e12b364f546484eeb0f8c8118a3400571f
EvenZeppa <zeppaeven.dev@gmail.com>
2025-03-06 19:01:34 +0100

    NORM utils and minishell.c

--- src/minishell.c
+++ src/minishell.c
@@ -2,20 +2,19 @@
 
 void	minishell(t_app *app)
 {
-	char *input;
+	char	*input;
 
 	input = NULL;
-	while (app->running && read_input(app, &input, NULL)) //si heredoc, on affiche "heredoc>"
+	while (app->running && read_input(app, &input, NULL))
 	{
-		if (check_entry(app, input))	//on regarde l'entre du prompt et si y'a pas de ctrl d
+		if (check_entry(app, input))
 		{
-			if (tokenize(app, input))	//remplie struct tokenizer en decoupant tous l'input correctement en prennant comptes les quotes
+			if (tokenize(app, input))
 				perror("tokenize");
 			app->first_node = prepare_ast(app, 0, app->tokenizer.t_count - 1);
-			if (!app->first_node)	//prepare les branches de l'ast
+			if (!app->first_node)
 				perror("prepare ast");
-			// print_ast(app->first_node, 0);
-			if (exec_ast(app, app->first_node)) //execute l'ast dans le bonne ordre
+			if (exec_ast(app, app->first_node))
 				perror("exec ast");
 			clean_tokenizer(app);
 			clean_ast(app->first_node);
